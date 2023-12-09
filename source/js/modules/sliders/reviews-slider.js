a1fbae247ccfb2359dbbdefe270a420975f1510b
dYAROSLAVy <yadsp@yandex.ru>
2023-12-09 21:11:41 +0300

    Добавляет блоки about, reviews, advantages и gallery

---
+++ source/js/modules/sliders/reviews-slider.js
@@ -0,0 +1,22 @@
+const reviewsSlider = () => {
+  const swiperWrapper = document.querySelector('[data-reviews-slider]');
+  const buttonPrev = document.querySelector('[data-reviews-slider-btn-prev]');
+  const buttonNext = document.querySelector('[data-reviews-slider-btn-next]');
+
+  return new window.Swiper(swiperWrapper, {
+    loop: false,
+    navigation: {
+      nextEl: buttonNext,
+      prevEl: buttonPrev,
+    },
+    breakpoints: {
+      1200: {
+        slidesPerView: 'auto',
+        spaceBetween: 120,
+        allowTouchMove: false,
+      },
+    },
+  });
+};
+
+export {reviewsSlider};
